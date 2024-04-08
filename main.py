a8f8a325bf228c46d1ba84130758f29684e60609
daichi2mori <daichi2mori@gmail.com>
2024-04-09 00:08:20 +0900

    🎨 style: コンポーネント化

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import flet as ft
 
 
+# ft.UserControlを継承してTodoAppクラスを作成
+# これにより状態をカプセル化している
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
+        self.add_button = ft.FloatingActionButton(
+            icon=ft.icons.ADD, on_click=self.add_clicked
+        )
+        self.tasks = ft.Column()
+
+        # ここが先ほどのview変数にあたります
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        self.add_button,
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
 def main(page: ft.Page):
-    def add_clicked(e):
-        # ft.Column内に描写するにはcontrols.appendが必要
-        # tasks_viewに追加することでview変数内に追加されていく
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        # page.update()でも画面は更新されるが、
-        # view.update()にすることで再描写の範囲を小さくしている
-        view.update()
-
-    new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
-    add_button = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked)
-
-    tasks_view = ft.Column()
-
-    # ft.Column内の要素は縦に並べられる
-    view = ft.Column(
-        width=600,
-        controls=[
-            # ft.Rowは横
-            ft.Row(
-                controls=[new_task, add_button],
-            ),
-            tasks_view,
-        ],
-    )
-
-    # ページ全体を中央揃えに設定
+    page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
+
+    todo = TodoApp()
+
+    page.add(todo)
 
 
 ft.app(target=main)
